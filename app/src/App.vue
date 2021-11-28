dbab88e06c6d84c195887fec00656dfd66ff180a
ravishankar <errvshkar@gmail.com>
2021-11-28 14:05:36 +0530

    v1.0 base patch

---
+++ app/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <div class='header'>
+      <h2>Letters</h2>
+    </div>
+    <div class='content'>
+      <List />
+    </div>
+    <div class='footer'>
+        <h2>by Company</h2>
+    </div>
+  </div>
+</template>
+
+<script>
+
+import List from './components/List.vue'
+
+export default {
+  name: 'App',
+  components: {
+    List,
+  }
+}
+</script>
+
+<style>
+</style>
---
+++ app/src/components/ListBar.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="list-bar">
+        <div>{{ content }} <span>{{ likes }}</span></div>
+        <hr>   
+    </div>
+</template>
+
+
+<script>
+export default {
+    name: "ListBar",
+    props: ["content", "likes"]
+}
+</script>
+
+<style>
+
+</style>
