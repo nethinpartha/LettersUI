8021bcbe241dcf3c4603de5663dff30861a86b89
ravishankar <errvshkar@gmail.com>
2021-12-03 08:48:38 +0530

    Likes, dislikes and comment support added

--- app/src/components/ListBar.vue
+++ app/src/components/ListBar.vue
@@ -1,9 +1,10 @@
 <template>
     <div class="list-bar"> 
-        <div> <a :href="viewUrl">{{ name }}</a> <span class="like-post">liked {{ likes }}</span></div>
+        <div> <a :href="viewUrl">{{ name }}</a> <span class="like-post">Liked {{ likes }}</span>
+        <span class="dislike-post">Disliked {{ dislikes }}</span></div>
         <div>{{ description  }}</div>  
         <br>
-        <br>
+        <br>    
     </div>
 </template>
 
@@ -12,7 +13,7 @@
 
 export default {
     name: "ListBar",
-    props: ["name", "description", "likes", "blog_id"],
+    props: ["name", "description", "likes", "dislikes", "blog_id"],
     data(){
         return {
          viewUrl: "/view/" + this.blog_id
--- app/src/views/Read.vue
+++ app/src/views/Read.vue
@@ -5,10 +5,18 @@
         <div v-for="(con, index) in contents.data" :key="index">
             <div>{{ con.value }}</div >
         </div>
+        <br>
         <div>
-            Likes {{ contents.updated.likes }} Comments {{ contents.updated.comments_count }}
+            <input type="button" value="like" @click="updateLikes()"><p>{{ contents.updated.likes }}</p>
+            <input type="button" value="Dislike" @click="updateDislikes()"><p>{{ contents.updated.dislikes }}</p>
         </div>
-        <p> <b>Comments Posted</b></p>
+        <div>
+            <p><b>Add comment..</b></p>
+            <textarea type="text" class="comment" placeholder="Write a comment" v-model="commentValue" 
+            @keyup.enter="updateComment()"></textarea>
+          <button @click="updateComment()" type="submit">post</button>
+        </div>
+        <p> <b>shared comments..</b></p>
         <div v-for="(comment, index) in contents.updated.comments" :key="index">
             <div>{{ comment }}</div>
         </div>
@@ -30,7 +38,8 @@ export default {
     },
     data(){
         return{
-            contents: []
+            contents: [],
+            commentValue: ""
         }
     },
     beforeMount(){
@@ -38,7 +47,33 @@ export default {
         let viewUrl = readBlogUrl + "?blog_id=" + this.$route.params.id
         axios.get(viewUrl).then(response => (this.contents = response.data))
     },
-    methods: {}
+    methods: {
+        updateComment(){
+            let updateBlogUrl = process.env.VUE_APP_BASE_URL + process.env.VUE_APP_APP_URL + process.env.VUE_APP_UPDATE_URL
+            let updateReq = {
+                "blog_id": this.$route.params.id,
+                "comment": this.commentValue,
+                "type": "comment"
+            }
+            axios.put(updateBlogUrl, updateReq).then(response => (this.$router.go(this.$router.currentRoute)))
+        },
+        updateLikes(){
+            let updateBlogUrl = process.env.VUE_APP_BASE_URL + process.env.VUE_APP_APP_URL + process.env.VUE_APP_UPDATE_URL
+            let updateReq = {
+                "blog_id": this.$route.params.id,
+                "type": "like"
+            }
+            axios.put(updateBlogUrl, updateReq).then(response => (this.$router.go(this.$router.currentRoute)))
+        },
+        updateDislikes(){
+            let updateBlogUrl = process.env.VUE_APP_BASE_URL + process.env.VUE_APP_APP_URL + process.env.VUE_APP_UPDATE_URL
+            let updateReq = {
+                "blog_id": this.$route.params.id,
+                "type": "dislike"
+            }
+            axios.put(updateBlogUrl, updateReq).then(response => (this.$router.go(this.$router.currentRoute)))
+        }
+    }
 }
 </script>
 
